93390dcca0bd639ad3e466b147d449b2fda263a8
Smak80 <smak-80@yandex.ru>
2020-02-18 10:37:27 +0300

    Добавьте файлы проекта.

---
+++ 2020-02-18L2k-DBlBf/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace _2020_02_18L2k_DBlBf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(541, 425);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(566, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ 2020-02-18L2k-DBlBf/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2020_02_18L2k_DBlBf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Image img = null;
+        private BufferedGraphicsContext bgc = BufferedGraphicsManager.Current;
+        private BufferedGraphics bg;
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            var b = new SolidBrush(Color.Purple);
+            if (img == null)
+            {
+                img = new Bitmap(
+                    panel1.Width, panel1.Height,
+                    panel1.CreateGraphics()
+                    );
+                var gimg = Graphics.FromImage(img);
+                bg = bgc.Allocate(panel1.CreateGraphics(), 
+                    new Rectangle(0, 0, panel1.Width, panel1.Height)
+                    );
+                gimg.Clear(Color.White);
+                gimg.FillPie(b, 
+                    new Rectangle(30,30,300,300), 
+                    0, 
+                    -105 
+                );
+            }
+            e.Graphics.DrawImage(img, 0, 0);
+        }
+
+        private bool paint = false;
+
+        private bool paintRect = false;
+        
+        private Point startPoint;
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                paint = true;
+            } else if (e.Button == MouseButtons.Right)
+            {
+                paintRect = true;
+            }
+            startPoint = new Point(e.X, e.Y);
+        }
+
+        private void panel1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                paint = false;
+                panel1.CreateGraphics().DrawImage(img, 0, 0);
+            } else if (e.Button == MouseButtons.Right)
+            {
+                paintRect = false;
+                var b = new SolidBrush(Color.GreenYellow);
+                var gimg = Graphics.FromImage(img);
+                var currentPoint = new Point(e.X, e.Y);
+                var sz = new Size(currentPoint.X-startPoint.X, 
+                    currentPoint.Y-startPoint.Y);
+                gimg.FillRectangle(b, new Rectangle(startPoint, sz));
+                panel1.CreateGraphics().DrawImage(img, 0, 0);
+                img.Save("kartinka.jpg", ImageFormat.Jpeg);
+            }
+        }
+
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (img == null) return;
+            var p = new Pen(Color.Blue);
+            var g = panel1.CreateGraphics();
+            var bgg = bg.Graphics;
+            var currentPoint = new Point(e.X, e.Y);
+
+            if (paint)
+            {
+                g.DrawLine(p, startPoint, currentPoint);
+                startPoint = currentPoint;
+            } else if (paintRect)
+            {
+                var sz = new Size(currentPoint.X-startPoint.X, 
+                    currentPoint.Y-startPoint.Y);
+                bgg.DrawImage(img, 0, 0);
+                bgg.DrawRectangle(p, new Rectangle(startPoint, sz));    
+                bg.Render();
+            }
+        }
+    }
+}
